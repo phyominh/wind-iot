04002d8a1c460e0ee51ddd2a9c442f832fa90d0c
phyominh <23469622+phyominh@users.noreply.github.com>
2020-08-12 23:26:20 +0630

    Added test for serial connection with GSM module

---
+++ tests/components/test_gsm_module.py
@@ -0,0 +1,10 @@
+import serial
+
+def test_at_command():
+    port = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
+    mesg = 'AT\r\n'.encode('utf-8')
+    port.write(mesg)
+    resp = port.read(10)
+    OK = 'OK'
+    OK_in_resp = resp.find(OK)
+    assert OK_in_resp != -1
