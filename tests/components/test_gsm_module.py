e261c50f935cd002a08295985d26e3e05bf55f01
phyominh <23469622+phyominh@users.noreply.github.com>
2020-08-14 23:13:59 +0630

    Added docstring to GSM module test

--- tests/components/test_gsm_module.py
+++ tests/components/test_gsm_module.py
@@ -1,6 +1,16 @@
 import serial
 
 def test_at_command():
+    """
+    AT Command to check the connection between Raspberry Pi and GSM module
+    If the connection is successful, the response will contain 'OK' string.
+    .............
+    Pin out guide
+    =============
+      * Raspberry Pi, Pin 8 (TXD) -> GSM Module, RX
+      * Raspberry Pi, Pin 10 (RXD) -> GSM Module, TX
+      * Raspberry Pi, Ground -> GSM Module, Ground
+    """
     port = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
     mesg = 'AT\r\n'.encode('utf-8')
     port.write(mesg)
