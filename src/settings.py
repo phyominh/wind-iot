63b98bb9fa347ccccb7bd6c57f44c9ff8b8846e9
phyominh <23469622+phyominh@users.noreply.github.com>
2020-08-15 11:15:46 +0630

    Added settings for AWS credentials

---
+++ src/settings.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# AWS credentials
+HOST = os.getenv('ENDPOINT')
+ROOT_CA = os.getenv('ROOT_CA')
+CERT = os.getenv('CERT')
+KEY = os.getenv('KEY')
+PORT = os.getenv('PORT')
+THING_NAME = os.getenv('THING_NAME')
+CLIENT_ID = os.getenv('CLIENT_ID')
+TOPIC = os.getenv('TOPIC')
